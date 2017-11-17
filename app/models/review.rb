f59e9244480ebd17f9b60da206b59f2526141434
Aaron Rohrbacher <arjazz@icloud.com>
2017-11-17 12:06:10 -0800

    validate that ratings are numbers between 1 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   validates :author, :presence => true
   validates :content_body, :presence => true
   validates :rating, :presence => true
+  validates_inclusion_of :rating, :in => 1..5
   validates :product_id, :presence => true
 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,9 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
+  it { should validate_presence_of :rating }
+  it { should_not allow_value('6').for(:rating) }
+  it { should allow_value('1').for(:rating) }
   it { should validate_presence_of :product_id }
 
 
