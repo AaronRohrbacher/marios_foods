fc13bc0c0d821d3b048325a085c9ec71a5942fcb
Aaron Rohrbacher <arjazz@icloud.com>
2017-11-17 12:27:37 -0800

    validate review content body character length between 50 and 250 characters

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
   validates :content_body, :presence => true
+  validates_length_of :content_body, :in => 50..250
   validates :rating, :presence => true
   validates :rating, :numericality => {:only_integer => true}
   validates_inclusion_of :rating, :in => 1..5
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
+  it { should allow_value('The quick brown fox jumps over the lazy dog and I dont care').for(:content_body) }
+  it { should_not allow_value('hello').for(:content_body)}
+  it { should_not allow_value('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into ').for(:content_body)}
+
   it { should validate_presence_of :rating }
   it { should validate_presence_of :rating }
   it { should_not allow_value('6').for(:rating) }
