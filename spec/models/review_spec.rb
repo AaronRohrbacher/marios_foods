f48573e8b8ecf2494d7f83d96f3348bfdd762b33
Aaron Rohrbacher <arjazz@icloud.com>
2017-11-17 12:16:04 -0800

    validate that a rating is an integer between 1 and 5 (no letters or decimals)

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,6 +6,8 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should validate_presence_of :rating }
   it { should_not allow_value('6').for(:rating) }
+  it { should_not allow_value('4.5').for(:rating) }
+  it { should_not allow_value('A').for(:rating) }
   it { should allow_value('1').for(:rating) }
   it { should validate_presence_of :product_id }
 
