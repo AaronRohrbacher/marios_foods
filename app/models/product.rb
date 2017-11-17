078f140ace8b14c8136f35f83d7b84f8d3b5b6f6
Aaron Rohrbacher <arjazz@icloud.com>
2017-11-17 13:51:37 -0800

    display most reviewd product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,13 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :country, :presence => true
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.country, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
+
 end
