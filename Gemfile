49bd8d8710e24c82be5949979450c660d8c0c3d4
Aaron Rohrbacher <arjazz@icloud.com>
2017-11-17 17:07:07 -0800

    add titles and little things

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'faker'
 gem 'jquery-rails'
+gem 'pg'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,7 +8,6 @@ describe Review do
   it { should_not allow_value('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into ').for(:content_body)}
 
   it { should validate_presence_of :rating }
-  it { should validate_presence_of :rating }
   it { should_not allow_value('6').for(:rating) }
   it { should_not allow_value('4.5').for(:rating) }
   it { should_not allow_value('A').for(:rating) }
